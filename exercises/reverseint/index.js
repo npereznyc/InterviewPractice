fce7dadd9a2d698e7588be3ad7016f883765e905
npereznyc <natalieperez84@gmail.com>
2024-01-25 11:22:51 -0600

    Added cleaner solution for reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,15 +9,19 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-    let result
+    // let result
     const revInt=parseInt(n.toString().split('').reverse().join(''))
+    
+    // if(Math.sign(n) === -1){
+    //     result = -revInt
+    // } else {
+    //     result = revInt
+    // }
+    // return result
 
-    if(Math.sign(n) === -1){
-        result = -revInt
-    } else {
-        result = revInt
-    }
-    return result
+    
+    return revInt * Math.sign(n)
+    //this is another way to arrive at the solution, without instantiating a new variable (result) and without using an if statement. If we multiply the reversed number by the the result of Math.sign(n), then we'll get a negative number if n is negative and a postive number if n is positive.
 }
 
 reverseInt(-53)
