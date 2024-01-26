5e0ae65a10d952238cfde454e6cfcb8b1d3b050d
npereznyc <natalieperez84@gmail.com>
2024-01-26 11:57:45 -0600

    added two solutions for maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,46 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    //instantiate empty object
+    //iterate over string to populate object
+    //object look up for greatest number - instantiate variable for greatest? update it's value?
+    let charObj={}
+    let max=0
+    let maxChar=''
+
+    for(let char of str) {
+        charObj[char] = charObj[char]+1 || 1
+    }
+
+    for(let char in charObj){
+        if(charObj[char]>max){
+            max=charObj[char]
+            maxChar=char
+        }
+    }
+    return maxChar
+
+    //alternate solution with similar time and space complexity:
+
+    // let charObj={}
+
+    // for(let char of str) {
+    //     charObj[char] = charObj[char]+1 || 1
+    // }
+
+    // const values = Object.values(charObj)
+
+    // const maxValue=values.reduce((max,currentValue)=> {
+    //     return (currentValue>max) ? currentValue : max
+    // }, values[0])
+
+    // return Object.keys(charObj).find(key =>charObj[key]===maxValue)
+   
+
+    
+
+}
+maxChar('abcdefghijklmnaaaaa')
 
 module.exports = maxChar;
