b06d24f0e862328815f1cc20f11a2c5faee65c2b
npereznyc <natalieperez84@gmail.com>
2024-01-29 12:13:27 -0600

    added third solution to anagrams that uses .sort() in a helper function

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,50 +8,62 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
+// function anagrams(stringA, stringB) {
     
     //if using helper function:
-    const objA=buildMap(stringA)
-    const objB=buildMap(stringB)
+    // const objA=buildMap(stringA)
+    // const objB=buildMap(stringB)
         
-    if(Object.keys(objA).length!=Object.keys(objB).length){
-        return false
-    }
-
-    //if not using helper function:
-    // const A = stringA.replace(/[^\w]/g,"").toLowerCase()
-    // const B = stringB.replace(/[^\w]/g,"").toLowerCase()
-
-    // if(A.length != B.length){
+    // if(Object.keys(objA).length!=Object.keys(objB).length){
     //     return false
     // }
 
-    // const objA = {}
-    // const objB = {}
+    //if not using helper function (still doing 3 total iterations with or without helper function):
+        // const A = stringA.replace(/[^\w]/g,"").toLowerCase()
+        // const B = stringB.replace(/[^\w]/g,"").toLowerCase()
+
+        // if(A.length != B.length){
+        //     return false
+        // }
+
+        // const objA = {}
+        // const objB = {}
     
-    // for(let char of A){
-    //     objA[char]= objA[char]+1 || 1
-    // }
-    // for(let char of B){
-    //     objB[char]= objB[char]+1 || 1
-    // }
+        // for(let char of A){
+        //     objA[char]= objA[char]+1 || 1
+        // }
+        // for(let char of B){
+        //     objB[char]= objB[char]+1 || 1
+        // }
 
-
-    for(let char in objA){
-        if(objA[char]!=objB[char]){
-            return false
-        }
-    }
-    return true
-}
+    // for(let char in objA){
+    //     if(objA[char]!=objB[char]){
+    //         return false
+    //     }
+    // }
+    // return true
+// }
 
 //helper function:
-function buildMap(str){
-    const charMap = {}
-    for(let char of str.replace(/[^\w]/g,'').toLowerCase()) {
-        charMap[char] = charMap[char]+1 || 1
-    }
-    return charMap
+// function buildMap(str){
+//     const charMap = {}
+//     for(let char of str.replace(/[^\w]/g,'').toLowerCase()) {
+//         charMap[char] = charMap[char]+1 || 1
+//     }
+//     return charMap
+// }
+
+
+//alternate solution, using a helper function that also sorts the strings and no hashmap:
+
+function anagrams(stringA, stringB) {
+    return cleanString(stringA)===cleanString(stringB) //will return true if this is true and fase if it is false.
+    
+}
+
+function cleanString(str){
+    return str.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('')
+    //because .sort() only works on arrays, we need to use .split(') to convert the string to an array and then .join('') to convert it back to a string
 }
 
 anagrams('One one', 'One one c')
