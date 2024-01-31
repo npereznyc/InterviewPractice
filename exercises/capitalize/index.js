0efc1be75700050113e17bf9f38d61ff4a2826ad
npereznyc <natalieperez84@gmail.com>
2024-01-31 11:55:10 -0600

    solved capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,18 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const arr=str.split(' ')
+    const newArr=[]
+
+    for(let i=0;i<arr.length;i++){
+        let capital = arr[i][0].toUpperCase()
+        let rest = arr[i].slice(1)
+        newArr.push(capital+rest)
+    }
+    return newArr.join(' ')
+}
+capitalize('hi there, how is it going?')
+
 
 module.exports = capitalize;
