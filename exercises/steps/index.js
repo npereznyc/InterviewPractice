990d7fec48f61e182d5f6f4f375e39d34f6dc4c2
npereznyc <natalieperez84@gmail.com>
2024-02-01 12:27:32 -0600

    solved steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for(let row=0; row<n; row++) {
+        let stair=''
+        for(let col=0; col<n; col++){
+            if(col<=row){
+                stair+='#'
+            } else {
+                stair+=' '
+            }
+        }       
+        console.log(stair)
+    }
+}
+
+steps(2)
 
 module.exports = steps;
