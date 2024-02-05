e1b491f56660510e654f53f8ad812174d511faf5
npereznyc <natalieperez84@gmail.com>
2024-02-05 12:15:58 -0600

    added recursive solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,46 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row=0, level='') {
+    //nested loop solution:
+    // const midpoint = Math.floor((n*2-1)/2) //length of longest row is n*2-1. we divide by 2 to get the midpoint; Math.floor() ensures we round down.
 
+    // for(let row=0; row<n; row++) {
+    //     let level=''
+    //     for(let col=0; col<n*2-1; col++){
+    //         console.log('row: ', row, 'col: ', col, 'mid: ', midpoint)
+    //         if(midpoint - row <= col && midpoint + row >= col){
+    //             level += '#'
+    //         } else {
+    //             level += ' '
+    //         }
+    //     }       
+    //     console.log(level)
+    // }
+
+    //recursive solution:
+
+    const midpoint = Math.floor((n*2-1)/2)
+
+    //base case:
+    if(n===row){
+        return
+    }
+
+    if(n*2-1===level.length){
+        console.log(level)
+        pyramid(n,row+1)
+        return
+    }
+
+    if(midpoint-row<=level.length && midpoint+row>=level.length){
+        level += '#'
+    }else {
+        level += ' '
+    }
+
+pyramid(n,row,level)
+
+}
+pyramid(3)
 module.exports = pyramid;
