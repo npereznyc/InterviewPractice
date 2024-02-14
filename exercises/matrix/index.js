db08b3c32832a8253fec329d1ee6968c5add5c3b
npereznyc <natalieperez84@gmail.com>
2024-02-14 11:17:39 -0500

    added solution for matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,58 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
 
+    //instantiating results array; this will be populated by the subarrays:
+    let results = []
+
+    //adding in n number of empty subarrays:
+    for(let i = 0; i<n; i++) {
+        results.push([])
+    }
+     console.log(results)
+
+    //instantiating counter that will keep track of what number we're pushing into the results array
+    let counter = 1
+
+    //declare variables for start and end columns and rows:
+    let startCol=0
+    let endCol=n-1
+    let startRow=0
+    let endRow=n-1
+
+    while(startCol<=endCol && startRow<=endRow){
+        //loop for top row:
+        for(let i=startCol; i<=endCol; i++){
+            results[startRow][i] = counter
+            counter++
+        }
+        startRow++
+
+        //loop for right column:
+        for(let i=startRow; i<= endRow; i++){
+            results[i][endCol]=counter
+            counter++
+        }
+        endCol--
+
+        //bottom row:
+        for(let i = endCol; i>=startCol; i--){
+            results[endRow][i]=counter
+            counter++
+        }
+        endRow--
+
+        //left column:
+
+        for(let i = endRow; i>= startRow; i--){
+            results[i][startCol]=counter
+            counter++
+        }
+        startCol++
+    }
+    console.log(results)
+    return results
+}
+matrix(3)
 module.exports = matrix;
